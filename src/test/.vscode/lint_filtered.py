6d70b00dbb465afc30d559081489ef83848effe9
moltenform.com(Ben Fisher) <amoltenform@gmail.com>
2025-08-31 16:55:52 -0700

    Get shinerainsevenlib library

---
+++ src/test/.vscode/lint_filtered.py
@@ -0,0 +1,84 @@
+import sys
+import json
+from shinerainsevenlib.standard import *
+
+
+def mainPylint():
+    args = [
+        '-m', 'pylint', '--rcfile', 'pyproject.toml', '--ignore-paths=.*OUTSIDE.*',
+        '--output-format=json2', '.'
+    ]
+    pyExe = sys.executable
+    args.insert(0, pyExe)
+    print(args)
+    retcode, stdout, stderr = files.run(args, throwOnFailure=False)
+    stdout = stdout.decode('utf-8')
+    events = json.loads(stdout)
+    events = events['messages']
+    for event in events:
+        line = formatOneLinePylint(event)
+        if 'nocpy_' in line:
+            continue
+        if "E1101: Instance of 'Bucket' has no" in line:
+            # skip this - we don't care about it for Bucket
+            continue
+        if "W0621: Redefining name 'fixture_" in line:
+            # skip this - we don't care about it for fixtures
+            continue
+        if "Access to a protected member _get of" in line:
+            # skip this - we don't care about it for fixtures
+            continue
+        if "Access to a protected member _get_id of" in line:
+            # skip this - we don't care about it for fixtures
+            continue
+
+        print(line)
+
+
+def mainRuff():
+    args = [
+        '-m', 'ruff', 'check', '--config=pyproject.toml', '--output-format=json',
+        '--exclude=.*OUTSIDE.*'
+    ]
+    if '--fix' in sys.argv:
+        args.append('--fix')
+
+    args.append('.')
+    pyExe = sys.executable
+    args.insert(0, pyExe)
+    print(args)
+    retcode, stdout, stderr = files.run(args, throwOnFailure=False)
+    stdout = stdout.decode('utf-8')
+    events = json.loads(stdout)
+    for event in events:
+        line = formatOneLineRuff(event)
+        if 'nocpy_' in line:
+            continue
+        
+        if "Access to a protected member _get_id of" in line:
+            # skip this - we don't care about it for fixtures
+            continue
+
+        print(line)
+
+
+def formatOneLineRuff(msg):
+    # example.py:32:67: W0640: An example message (the-warning-type)
+    return f"{msg['filename']}:{msg['location'].get('row')}:{msg['location'].get('column')}: {msg['code']} {msg['message']} ({msg.get('url', '').split('/')[-1]})"
+
+
+def formatOneLinePylint(msg):
+    # example.py:32:67: W0640: An example message (the-warning-type)
+    return f"{msg['path']}:{msg['line']}:{msg['column']}: {msg['messageId']}: {msg['message']} ({msg['symbol']})"
+
+
+if __name__ == '__main__':
+    try:
+        files.delete('__init__.py')
+        if '--ruff' in sys.argv:
+            mainRuff()
+        else:
+            mainPylint()
+    finally:
+        files.writeAll('__init__.py', '')
+
--- src/test/test_sfarkxtc.py
+++ src/test/test_sfarkxtc.py
@@ -7,8 +7,14 @@ import sys
 import os
 import hashlib
 import time
-sys.path.append('bn_python_common.zip')
-from bn_python_common import *
+
+try:
+    from shinerainsevenlib.standard import *
+    from shinerainsevenlib.core import *
+except ImportError:
+    raise ImportError('Please first run ' + 
+        '`pip install shinerainsevenlib` to run these tests.')
+
 
 # for 9 large soundfonts, average of 5 runs:
 # with Duma disabled, sfark compression 2, not-hardened: 13.5 seconds wall time
@@ -30,11 +36,11 @@ sfarkxtc_binary = os.path.abspath(sfarkxtc_binary)
 
 def getActualChecksums():
     actualChecksums = {}
-    for f, short in files.listfiles(originals):
+    for f, short in files.listFiles(originals):
         actualChecksums[short] = hash(f)
     
     licenseFile = files.join(testdir, 'alicense.txt')
-    actualChecksums[files.getname(licenseFile)] = hash(licenseFile)
+    actualChecksums[files.getName(licenseFile)] = hash(licenseFile)
     return actualChecksums
 
 def hash(s):
@@ -50,20 +56,19 @@ def hash(s):
         return files.computeHash(s, 'md5')
 
 def runSfark(f):
-    import os
-    a, b = os.path.splitext(files.getname(f))
+    a, b = os.path.splitext(files.getName(f))
     if not b.lower().endswith('.sfark'):
         trace('found a leftover file', f)
         return
     wantOutname = a + '__out__.sf2'
     assertTrue(files.exists(sfarkxtc_binary), 'binary not found', sfarkxtc_binary)
     if needToChdir:
-        os.chdir(files.getparent(f))
-        fIn = files.getname(f)
+        os.chdir(files.getParent(f))
+        fIn = files.getName(f)
         fOut = wantOutname
     else:
         fIn = f
-        fOut = files.join(files.getparent(f), wantOutname)
+        fOut = files.join(files.getParent(f), wantOutname)
     trace(fIn, fOut)
     args = [sfarkxtc_binary, fIn, fOut, '--quiet']
     files.run(args)
@@ -73,63 +78,63 @@ def withNewExt(s, newext):
     return a + newext
 
 def goDir(d, actualChecksums):
-    trace('checking directory:', files.getname(d))
-    shouldContainLicense = not '0license' in files.getname(d)
-    shouldContainNotes = not '0notes' in files.getname(d)
+    trace('checking directory:', files.getName(d))
+    shouldContainLicense = not '0license' in files.getName(d)
+    shouldContainNotes = not '0notes' in files.getName(d)
     
-    for f, short in files.listfiles(d):
+    for f, short in files.listFiles(d):
         if not short.lower().endswith('.sfark') and not short.endswith('.dll'):
             warn('about to delete temporary file ' + f + ' ok?')
             files.delete(f)
-    prevItemsInDir = {short:True for f, short in files.listfiles(d)}
-    workToDo = [(f, short) for f, short in files.listfiles(d)]
+    prevItemsInDir = {short:True for f, short in files.listFiles(d)}
+    workToDo = [(f, short) for f, short in files.listFiles(d)]
     
     for f, short in workToDo:
-        if short.endswith('.dll'): continue
+        if short.endswith('.dll'):
+            continue
+        
         assertTrue(f.endswith('.sfArk'), f)
         runSfark(f)
-        nowItemsInDir = {short:True for f, short in files.listfiles(d)}
+        nowItemsInDir = {short:True for f, short in files.listFiles(d)}
         newItems = {k:v for k,v in nowItemsInDir.items() if k not in prevItemsInDir}
         expectCountNewItems = 1 + (1 if shouldContainLicense else 0) + (1 if shouldContainNotes else 0)
         assertEq(expectCountNewItems, len(newItems), newItems)
         
         expectSf2 = withNewExt(f, '__out__.sf2')
         trace('checking hash for', expectSf2)
-        assertTrue(files.getname(expectSf2) in newItems, expectSf2, newItems)
-        nameToCompare = files.getname(expectSf2).replace('__out__', '')
-        assertTrue(files.getname(nameToCompare) in actualChecksums, expectSf2, actualChecksums)
+        assertTrue(files.getName(expectSf2) in newItems, expectSf2, newItems)
+        nameToCompare = files.getName(expectSf2).replace('__out__', '')
+        assertTrue(files.getName(nameToCompare) in actualChecksums, expectSf2, actualChecksums)
         trace(hash(expectSf2), actualChecksums[nameToCompare])
         assertEq(hash(expectSf2), actualChecksums[nameToCompare], f)
-        files.deletesure(expectSf2)
+        files.deleteSure(expectSf2)
         
         if shouldContainNotes:
             expectNotes = withNewExt(f, '__out__.txt')
             trace('checking hash for', expectNotes)
-            assertTrue(files.getname(expectNotes) in newItems, expectNotes, newItems)
-            nameToCompare = files.getname(expectNotes).replace('__out__', '')
-            assertTrue(files.getname(nameToCompare) in actualChecksums, expectNotes, actualChecksums)
+            assertTrue(files.getName(expectNotes) in newItems, expectNotes, newItems)
+            nameToCompare = files.getName(expectNotes).replace('__out__', '')
+            assertTrue(files.getName(nameToCompare) in actualChecksums, expectNotes, actualChecksums)
             trace(hash(expectNotes), actualChecksums[nameToCompare])
             assertEq(hash(expectNotes), actualChecksums[nameToCompare], f)
-            files.deletesure(expectNotes)
+            files.deleteSure(expectNotes)
             
         if shouldContainLicense:
             expectLicense = withNewExt(f, '__out__.license.txt')
             trace('checking hash for', expectLicense)
-            assertTrue(files.getname(expectLicense) in newItems, expectLicense, newItems)
+            assertTrue(files.getName(expectLicense) in newItems, expectLicense, newItems)
             assertTrue('alicense.txt' in actualChecksums, expectLicense, actualChecksums)
-            trace(hash(expectLicense), actualChecksums[files.getname('alicense.txt')])
-            assertEq(hash(expectLicense), actualChecksums[files.getname('alicense.txt')], f)
-            files.deletesure(expectLicense)
+            trace(hash(expectLicense), actualChecksums[files.getName('alicense.txt')])
+            assertEq(hash(expectLicense), actualChecksums[files.getName('alicense.txt')], f)
+            files.deleteSure(expectLicense)
 
 def go():
     actualChecksums = getActualChecksums()
     start = time.time()
     
-    for f, short in files.listdirs(testdir):
-
-
+    for f, _short in files.listDirs(testdir):
         for dll in sfarkxtc_shared:
-            dllsrc = files.join(files.getparent(sfarkxtc_binary), dll)
+            dllsrc = files.join(files.getParent(sfarkxtc_binary), dll)
             if not files.exists(dllsrc):
                 warn('file not found: ' + dllsrc)
             dlldest = files.join(f, dll)
